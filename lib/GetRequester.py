b67c8de2a520ade189e0eeadeda4002854ab0aee
rwparrish <60946750+rwparrish@users.noreply.github.com>
2023-08-01 18:34:42 -0600

    YAY
    q
    exit()
    quit()
    q
    q
    q
    '

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,19 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
+        
 
     def get_response_body(self):
-        pass
+        
+        response = requests.get(self.url)
+        return response.content
+
 
     def load_json(self):
-        pass
+        response_list = json.loads(self.get_response_body())
+        return response_list
